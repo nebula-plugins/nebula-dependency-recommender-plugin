acfcb571bd0438f22149031c5c74340518aff1ae
Jon Schneider <joschneider@netflix.com>
2014-11-15 16:38:23 -0800

    Added test for custom recommendation provider

---
+++ src/test/groovy/netflix/nebula/dependency/recommendations/provider/CustomRecommendationProviderSpec.groovy
@@ -0,0 +1,13 @@
+package netflix.nebula.dependency.recommendations.provider
+
+import spock.lang.Specification
+
+class CustomRecommendationProviderSpec extends Specification {
+    def 'can provide a closure that is wholly responsible for determining versions'() {
+        when:
+        def recommender = new CustomRecommendationProvider({ org, name -> '1.0' })
+
+        then:
+        recommender.getVersion('commons-logging', 'commons-logging') == '1.0'
+    }
+}
