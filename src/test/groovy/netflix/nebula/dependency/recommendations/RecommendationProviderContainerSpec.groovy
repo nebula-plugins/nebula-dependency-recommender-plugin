1fc7ccff573ccec5f45d9faa857ae489a8832556
Jon Schneider <joschneider@netflix.com>
2014-11-15 19:04:41 -0800

    added more tests to provider container

--- src/test/groovy/netflix/nebula/dependency/recommendations/RecommendationProviderContainerSpec.groovy
+++ src/test/groovy/netflix/nebula/dependency/recommendations/RecommendationProviderContainerSpec.groovy
@@ -1,6 +1,6 @@
 package netflix.nebula.dependency.recommendations
 
-import netflix.nebula.dependency.recommendations.provider.PropertyFileRecommendationProvider
+import org.gradle.api.Project
 import org.gradle.testfixtures.ProjectBuilder
 import org.junit.Rule
 import org.junit.rules.TemporaryFolder
@@ -9,14 +9,18 @@ import spock.lang.Specification
 class RecommendationProviderContainerSpec extends Specification {
     @Rule TemporaryFolder projectDir
 
-    def 'version recommendations are given in the order that the providers are specified'() {
-        setup:
-        def project = ProjectBuilder.builder().build()
+    Project project
+
+    def setup() {
+        project = ProjectBuilder.builder().build()
         project.apply plugin: 'java'
         project.apply plugin: 'nebula-dependency-recommendations'
 
         project.repositories { mavenCentral() }
+    }
 
+    def 'version recommendations are given in the order that the providers are specified'() {
+        setup:
         project.dependencyRecommendations {
             map recommendations: ['commons-logging:commons-logging': '1.1']
             map recommendations: ['commons-logging:commons-logging': '1.2', 'com.google.guava:guava': '18.0']
@@ -30,5 +34,37 @@ class RecommendationProviderContainerSpec extends Specification {
 
         then:
         project.configurations.compile.resolvedConfiguration.firstLevelModuleDependencies.collect { it.moduleVersion } == ['1.1', '18.0']
+    }
+
+    def 'transitive dependencies of providers are not calculated and therefore have no effect'() {
+        setup:
+        project.dependencyRecommendations {
+            map recommendations: ['commons-logging:commons-logging': '1.1']
+            map recommendations: ['logkit:logkit': '2.0']
+        }
+
+        when:
+        project.dependencies {
+            compile 'commons-logging:commons-logging'
+            compile 'logkit:logkit'
+        }
+
+        then:
+        project.configurations.compile.resolvedConfiguration.firstLevelModuleDependencies.collect { it.moduleVersion } == ['1.1', '2.0']
+    }
+
+    def 'dependencies that already have versions are not overriden by providers'() {
+        setup:
+        project.dependencyRecommendations {
+            map recommendations: ['commons-logging:commons-logging': '1.1']
+        }
+
+        when:
+        project.dependencies {
+            compile 'commons-logging:commons-logging:1.0'
+        }
+
+        then:
+        project.configurations.compile.resolvedConfiguration.firstLevelModuleDependencies.collect { it.moduleVersion } == ['1.0']
     }
 }
