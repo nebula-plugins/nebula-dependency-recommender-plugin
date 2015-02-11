18e0aae3ecfe096eec91c21e860e2d188e842e52
Jon Schneider <joschneider@netflix.com>
2015-02-11 15:30:02 -0800

    BOM files that specify a non-relative parent pom are resolvable

--- src/main/groovy/netflix/nebula/dependency/recommender/provider/MavenBomRecommendationProvider.java
+++ src/main/groovy/netflix/nebula/dependency/recommender/provider/MavenBomRecommendationProvider.java
@@ -1,19 +1,31 @@
 package netflix.nebula.dependency.recommender.provider;
 
 import org.gradle.api.Project;
+import org.gradle.api.artifacts.repositories.ArtifactRepository;
+import org.gradle.api.artifacts.repositories.MavenArtifactRepository;
+import org.gradle.api.publish.maven.MavenArtifact;
 import org.gradle.mvn3.org.apache.maven.model.Dependency;
 import org.gradle.mvn3.org.apache.maven.model.Model;
+import org.gradle.mvn3.org.apache.maven.model.Repository;
 import org.gradle.mvn3.org.apache.maven.model.building.*;
 import org.gradle.mvn3.org.apache.maven.model.interpolation.StringSearchModelInterpolator;
 import org.gradle.mvn3.org.apache.maven.model.path.DefaultPathTranslator;
 import org.gradle.mvn3.org.apache.maven.model.path.DefaultUrlNormalizer;
+import org.gradle.mvn3.org.apache.maven.model.resolution.InvalidRepositoryException;
+import org.gradle.mvn3.org.apache.maven.model.resolution.ModelResolver;
+import org.gradle.mvn3.org.apache.maven.model.resolution.UnresolvableModelException;
 import org.gradle.mvn3.org.codehaus.plexus.interpolation.MapBasedValueSource;
 import org.gradle.mvn3.org.codehaus.plexus.interpolation.PropertiesBasedValueSource;
 import org.gradle.mvn3.org.codehaus.plexus.interpolation.ValueSource;
+import org.gradle.mvn3.org.sonatype.aether.util.DefaultRepositorySystemSession;
+import org.gradle.mvn3.org.sonatype.aether.util.artifact.DefaultArtifactType;
+import org.gradle.mvn3.org.sonatype.aether.util.artifact.DefaultArtifactTypeRegistry;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -26,23 +38,75 @@ public class MavenBomRecommendationProvider extends FileBasedRecommendationProvi
         super(project);
     }
 
+    public void foo() {
+        DefaultRepositorySystemSession session = new DefaultRepositorySystemSession();
+        DefaultArtifactTypeRegistry stereotypes = new DefaultArtifactTypeRegistry();
+        stereotypes.add(new DefaultArtifactType("pom"));
+        session.setArtifactTypeRegistry(stereotypes);
+    }
+
+    private class SimpleModelSource implements ModelSource {
+        InputStream in;
+
+        public SimpleModelSource(InputStream in) {
+            this.in = in;
+        }
+
+        @Override
+        public InputStream getInputStream() throws IOException {
+            return in;
+        }
+
+        @Override
+        public String getLocation() {
+            return null;
+        }
+    }
+
     @Override
     public String getVersion(String org, String name) throws Exception {
         if(recommendations == null) {
             recommendations = new HashMap<>();
+
             DefaultModelBuildingRequest request = new DefaultModelBuildingRequest();
 
-            request.setModelSource(new ModelSource() {
+            request.setModelResolver(new ModelResolver() {
                 @Override
-                public InputStream getInputStream() throws IOException {
-                    return getInput();
+                public ModelSource resolveModel(String groupId, String artifactId, String version) throws UnresolvableModelException {
+                    String relativeUrl = "";
+                    for(String groupIdPart : groupId.split("\\."))
+                        relativeUrl += groupIdPart + "/";
+                    relativeUrl += artifactId + "/" + version + "/" + artifactId + "-" + version + ".pom";
+                    try {
+                        // try to find the parent pom in each maven repository specified in the gradle file
+                        for(ArtifactRepository repo : project.getRepositories()) {
+                            if(!(repo instanceof MavenArtifactRepository))
+                                continue;
+                            URL url = new URL(((MavenArtifactRepository) repo).getUrl().toString() + "/" + relativeUrl);
+                            try {
+                                return new SimpleModelSource(url.openStream());
+                            } catch (IOException e) {
+                                // try the next repo
+                            }
+                        }
+                    } catch (MalformedURLException e) {
+                        throw new RuntimeException(e); // should never happen
+                    }
+                    return null;
                 }
 
                 @Override
-                public String getLocation() {
-                    return null;
+                public void addRepository(Repository repository) throws InvalidRepositoryException {
+                    // do nothing
+                }
+
+                @Override
+                public ModelResolver newCopy() {
+                    return this; // do nothing
                 }
             });
+
+            request.setModelSource(new SimpleModelSource(getInput()));
 
             DefaultModelBuilder modelBuilder = new DefaultModelBuilderFactory().newInstance();
             modelBuilder.setModelInterpolator(new ProjectPropertiesModelInterpolator(project));
@@ -76,9 +140,9 @@ public class MavenBomRecommendationProvider extends FileBasedRecommendationProvi
 
         public List<ValueSource> createValueSources(Model model, File projectDir, ModelBuildingRequest request, ModelProblemCollector collector) {
             List<ValueSource> sources = new ArrayList<>();
-            sources.add(new MapBasedValueSource(project.getProperties()));
-            sources.add(new PropertiesBasedValueSource(System.getProperties()));
             sources.addAll(super.createValueSources(model, projectDir, request, collector));
+            sources.add(new PropertiesBasedValueSource(System.getProperties()));
+            sources.add(new MapBasedValueSource(project.getProperties()));
             return sources;
         }
     }
--- src/test/groovy/netflix/nebula/dependency/recommender/provider/MavenBomRecommendationProviderSpec.groovy
+++ src/test/groovy/netflix/nebula/dependency/recommender/provider/MavenBomRecommendationProviderSpec.groovy
@@ -19,28 +19,28 @@ class MavenBomRecommendationProviderSpec extends Specification {
         sample.mkdirs()
         def sampleFile = new File(sample, 'recommender-1.0.pom')
         sampleFile << '''
-<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-     xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
-  <modelVersion>4.0.0</modelVersion>
-  <groupId>sample</groupId>
-  <artifactId>recommender</artifactId>
-  <version>1.0</version>
+            <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                 xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+              <modelVersion>4.0.0</modelVersion>
+              <groupId>sample</groupId>
+              <artifactId>recommender</artifactId>
+              <version>1.0</version>
 
-  <dependencyManagement>
-    <dependencies>
-      <dependency>
-        <groupId>commons-logging</groupId>
-        <artifactId>commons-logging</artifactId>
-        <version>1.1.1</version>
-      </dependency>
-      <dependency>
-        <groupId>commons-configuration</groupId>
-        <artifactId>commons-configuration</artifactId>
-        <version>${commons.version}</version>
-      </dependency>
-    </dependencies>
-  </dependencyManagement>
-</project>
+              <dependencyManagement>
+                <dependencies>
+                  <dependency>
+                    <groupId>commons-logging</groupId>
+                    <artifactId>commons-logging</artifactId>
+                    <version>1.1.1</version>
+                  </dependency>
+                  <dependency>
+                    <groupId>commons-configuration</groupId>
+                    <artifactId>commons-configuration</artifactId>
+                    <version>${commons.version}</version>
+                  </dependency>
+                </dependencies>
+              </dependencyManagement>
+            </project>
         '''
 
         project.repositories { maven { url repo } }
@@ -55,5 +55,53 @@ class MavenBomRecommendationProviderSpec extends Specification {
         then:
         recommendations.getVersion('commons-logging', 'commons-logging') == '1.1.1'
         recommendations.getVersion('commons-configuration', 'commons-configuration') == '1.1.2'
+    }
+
+    def 'bom files that specify a non-relative parent pom are resolvable'() {
+        setup:
+        def project = ProjectBuilder.builder().build()
+        project.apply plugin: 'java'
+        project.apply plugin: 'nebula-dependency-recommender'
+
+        def repo = projectDir.newFolder('repo')
+        def sample = new File(repo, 'sample/recommender/1.1.1')
+        sample.mkdirs()
+        def sampleFile = new File(sample, 'recommender-1.1.1.pom')
+        sampleFile << '''
+            <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                 xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+              <modelVersion>4.0.0</modelVersion>
+              <parent>
+                <artifactId>oss-parent</artifactId>
+                <groupId>org.sonatype.oss</groupId>
+                <version>7</version>
+              </parent>
+              <groupId>sample</groupId>
+              <artifactId>recommender</artifactId>
+              <version>1.1.1</version>
+
+              <dependencyManagement>
+                <dependencies>
+                  <dependency>
+                    <groupId>commons-logging</groupId>
+                    <artifactId>commons-logging</artifactId>
+                    <version>${project.version}</version>
+                  </dependency>
+                </dependencies>
+              </dependencyManagement>
+            </project>
+        '''
+
+        project.repositories {
+            maven { url repo }
+            mavenCentral()
+        }
+
+        when:
+        def recommendations = new MavenBomRecommendationProvider(project)
+        recommendations.setModule('sample:recommender:1.1.1')
+
+        then:
+        recommendations.getVersion('commons-logging', 'commons-logging') == '1.1.1'
     }
 }
