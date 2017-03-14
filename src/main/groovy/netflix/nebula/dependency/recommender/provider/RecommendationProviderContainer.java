70903bd84f6bfaf72deeb11aa95d147fa63e162c
Rob Spieldenner <rob.spieldenner@gmail.com>
2017-03-14 15:34:28 -0700

    Update license header for modified files

--- src/main/groovy/netflix/nebula/dependency/recommender/provider/RecommendationProviderContainer.java
+++ src/main/groovy/netflix/nebula/dependency/recommender/provider/RecommendationProviderContainer.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2014-2017 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package netflix.nebula.dependency.recommender.provider;
 
 import com.netflix.nebula.dependencybase.DependencyManagement;
@@ -7,7 +22,6 @@ import netflix.nebula.dependency.recommender.RecommendationStrategies;
 import org.gradle.api.Action;
 import org.gradle.api.Namer;
 import org.gradle.api.Project;
-import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.internal.ClosureBackedAction;
 import org.gradle.api.internal.ConfigureByMapAction;
 import org.gradle.api.internal.DefaultNamedDomainObjectList;
