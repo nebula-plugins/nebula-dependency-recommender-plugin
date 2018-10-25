a6709b2c74dee23e364a606de3580fcaea3bc49c
Martin Chalupa <mchalupa@netflix.com>
2018-10-25 13:53:29 -0700

    Remove --scan from travis build script so it doesnt autoapply build scan plugin

--- gradle/buildViaTravis.sh
+++ gradle/buildViaTravis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This script will build the project.
 
-SWITCHES="--info --stacktrace --parallel --scan"
+SWITCHES="--info --stacktrace --parallel"
 
 GRADLE_VERSION=$(./gradlew -version | grep Gradle | cut -d ' ' -f 2)
 
