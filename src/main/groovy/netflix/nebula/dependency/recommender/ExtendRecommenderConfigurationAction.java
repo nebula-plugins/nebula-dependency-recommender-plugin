04cdb71002188283a29720c5e2dc50ad5cf92ffb
Martin Chalupa <mchalupa@netflix.com>
2018-10-25 17:22:28 -0700

    Only unresolved configurations will receive recommendation from BOM

--- src/main/groovy/netflix/nebula/dependency/recommender/ExtendRecommenderConfigurationAction.java
+++ src/main/groovy/netflix/nebula/dependency/recommender/ExtendRecommenderConfigurationAction.java
@@ -4,8 +4,13 @@ import netflix.nebula.dependency.recommender.provider.RecommendationProviderCont
 import org.gradle.api.Action;
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.Configuration;
+import org.gradle.api.logging.Logger;
+import org.gradle.api.logging.Logging;
 
 public class ExtendRecommenderConfigurationAction implements Action<Configuration> {
+
+    private Logger logger = Logging.getLogger(ExtendRecommenderConfigurationAction.class);
+
     private final Configuration bom;
     private final Project project;
     private final RecommendationProviderContainer container;
@@ -21,12 +26,17 @@ public class ExtendRecommenderConfigurationAction implements Action<Configuratio
         if (container.getExcludedConfigurations().contains(configuration.getName())) {
             return;
         }
-        Configuration toExtend = bom;
-        if (!project.getRootProject().equals(project)) {
-            toExtend = bom.copy();
-            toExtend.setVisible(false);
-            project.getConfigurations().add(toExtend);
+
+        if (configuration.getState() == Configuration.State.UNRESOLVED) {
+            Configuration toExtend = bom;
+            if (!project.getRootProject().equals(project)) {
+                toExtend = bom.copy();
+                toExtend.setVisible(false);
+                project.getConfigurations().add(toExtend);
+            }
+            configuration.extendsFrom(toExtend);
+        } else {
+            logger.info("Configuration '" + configuration.getName() + "' has already been resolved and cannot be included for recommendation");
         }
-        configuration.extendsFrom(toExtend);
     }
 }
