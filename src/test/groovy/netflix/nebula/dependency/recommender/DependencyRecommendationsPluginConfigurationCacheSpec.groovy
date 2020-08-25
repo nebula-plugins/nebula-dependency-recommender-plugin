900b54fe6baa83ccbe3ff24f033f1cea03f53f90
Roberto Perez Alcolea <rperezalcolea@netflix.com>
2020-08-25 11:27:46 -0700

    Gradle 6.6.1

---
+++ src/test/groovy/netflix/nebula/dependency/recommender/DependencyRecommendationsPluginConfigurationCacheSpec.groovy
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2016-2017 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package netflix.nebula.dependency.recommender
+
+import nebula.test.IntegrationTestKitSpec
+import nebula.test.dependencies.DependencyGraphBuilder
+import nebula.test.dependencies.GradleDependencyGenerator
+import nebula.test.dependencies.maven.ArtifactType
+import nebula.test.dependencies.maven.Pom
+import nebula.test.dependencies.repositories.MavenRepo
+
+class DependencyRecommendationsPluginConfigurationCacheSpec extends IntegrationTestKitSpec  {
+
+    def 'provide recommendation via configuration'() {
+        def repo = new MavenRepo()
+        repo.root = new File(projectDir, 'build/bomrepo')
+        def pom = new Pom('test.nebula.bom', 'testbom', '1.0.0', ArtifactType.POM)
+        pom.addManagementDependency('test.nebula', 'foo', '1.0.0')
+        repo.poms.add(pom)
+        repo.generate()
+        def graph = new DependencyGraphBuilder()
+                .addModule('test.nebula:foo:1.0.0')
+                .build()
+        def generator = new GradleDependencyGenerator(graph)
+        generator.generateTestMavenRepo()
+
+        buildFile << """\
+            plugins {
+                id 'nebula.dependency-recommender'
+                id 'java'
+            }
+
+            repositories {
+                maven { url '${repo.root.absoluteFile.toURI()}' }
+                ${generator.mavenRepositoryBlock}
+            }
+
+            dependencies {
+                nebulaRecommenderBom 'test.nebula.bom:testbom:1.0.0@pom'
+                implementation 'test.nebula:foo'
+            }
+            """.stripIndent()
+
+        when:
+        def result = runTasks('--configuration-cache', 'dependencies')
+
+        then:
+        result.output.contains 'test.nebula:foo -> 1.0.0'
+    }
+
+}
