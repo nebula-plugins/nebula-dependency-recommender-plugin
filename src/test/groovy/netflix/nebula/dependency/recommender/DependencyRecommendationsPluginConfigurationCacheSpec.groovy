d2d6200acbb7b8858df57fd3582a4e3a88cb9c49
Roberto Perez Alcolea <rperezalcolea@netflix.com>
2025-12-01 20:00:54 -0800

    Add more configuration cache tests

--- src/test/groovy/netflix/nebula/dependency/recommender/DependencyRecommendationsPluginConfigurationCacheSpec.groovy
+++ src/test/groovy/netflix/nebula/dependency/recommender/DependencyRecommendationsPluginConfigurationCacheSpec.groovy
@@ -58,4 +58,134 @@ class DependencyRecommendationsPluginConfigurationCacheSpec extends IntegrationT
         runTasks('--configuration-cache', 'dependencies')
     }
 
+    def 'strictMode property works with configuration cache'() {
+        def repo = new MavenRepo()
+        repo.root = new File(projectDir, 'build/bomrepo')
+        def pom = new Pom('test.nebula.bom', 'testbom', '1.0.0', ArtifactType.POM)
+        pom.addManagementDependency('test.nebula', 'foo', '1.0.0')
+        repo.poms.add(pom)
+        repo.generate()
+        def graph = new DependencyGraphBuilder()
+                .addModule('test.nebula:foo:1.0.0')
+                .build()
+        def generator = new GradleDependencyGenerator(graph)
+        generator.generateTestMavenRepo()
+
+        buildFile << """\
+            plugins {
+                id 'com.netflix.nebula.dependency-recommender'
+                id 'java'
+            }
+
+            repositories {
+                maven { url = '${repo.root.absoluteFile.toURI()}' }
+                ${generator.mavenRepositoryBlock}
+            }
+
+            dependencyRecommendations {
+                strictMode.set(false)
+            }
+
+            dependencies {
+                nebulaRecommenderBom 'test.nebula.bom:testbom:1.0.0@pom'
+                implementation 'test.nebula:foo'
+            }
+            """.stripIndent()
+
+        when:
+        def result1 = runTasks('--configuration-cache', 'dependencies')
+        def result2 = runTasks('--configuration-cache', 'dependencies')
+
+        then:
+        result1.output.contains('Calculating task graph')
+        result2.output.contains('Reusing configuration cache')
+    }
+
+    def 'eagerlyResolve property works with configuration cache'() {
+        def repo = new MavenRepo()
+        repo.root = new File(projectDir, 'build/bomrepo')
+        def pom = new Pom('test.nebula.bom', 'testbom', '1.0.0', ArtifactType.POM)
+        pom.addManagementDependency('test.nebula', 'foo', '1.0.0')
+        repo.poms.add(pom)
+        repo.generate()
+        def graph = new DependencyGraphBuilder()
+                .addModule('test.nebula:foo:1.0.0')
+                .build()
+        def generator = new GradleDependencyGenerator(graph)
+        generator.generateTestMavenRepo()
+
+        buildFile << """\
+            plugins {
+                id 'com.netflix.nebula.dependency-recommender'
+                id 'java'
+            }
+
+            repositories {
+                maven { url = '${repo.root.absoluteFile.toURI()}' }
+                ${generator.mavenRepositoryBlock}
+            }
+
+            dependencyRecommendations {
+                eagerlyResolve.set(true)
+            }
+
+            dependencies {
+                nebulaRecommenderBom 'test.nebula.bom:testbom:1.0.0@pom'
+                implementation 'test.nebula:foo'
+            }
+            """.stripIndent()
+
+        when:
+        def result1 = runTasks('--configuration-cache', 'dependencies')
+        def result2 = runTasks('--configuration-cache', 'dependencies')
+
+        then:
+        result1.output.contains('Calculating task graph')
+        result2.output.contains('Reusing configuration cache')
+    }
+
+    def 'excludedConfigurations SetProperty works with configuration cache'() {
+        def repo = new MavenRepo()
+        repo.root = new File(projectDir, 'build/bomrepo')
+        def pom = new Pom('test.nebula.bom', 'testbom', '1.0.0', ArtifactType.POM)
+        pom.addManagementDependency('test.nebula', 'foo', '1.0.0')
+        repo.poms.add(pom)
+        repo.generate()
+        def graph = new DependencyGraphBuilder()
+                .addModule('test.nebula:foo:1.0.0')
+                .build()
+        def generator = new GradleDependencyGenerator(graph)
+        generator.generateTestMavenRepo()
+
+        buildFile << """\
+            plugins {
+                id 'com.netflix.nebula.dependency-recommender'
+                id 'java'
+            }
+
+            repositories {
+                maven { url = '${repo.root.absoluteFile.toURI()}' }
+                ${generator.mavenRepositoryBlock}
+            }
+
+            dependencyRecommendations {
+                excludedConfigurations.add('testCompileClasspath')
+                excludedConfigurationPrefixes.add('test')
+            }
+
+            dependencies {
+                nebulaRecommenderBom 'test.nebula.bom:testbom:1.0.0@pom'
+                implementation 'test.nebula:foo'
+            }
+            """.stripIndent()
+
+        when:
+        def result1 = runTasks('--configuration-cache', 'dependencies')
+        def result2 = runTasks('--configuration-cache', 'dependencies')
+
+        then:
+        result1.output.contains('Calculating task graph')
+        result2.output.contains('Reusing configuration cache')
+    }
+
 }
